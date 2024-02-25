5be2ff5d86cf09269d9e6ca27c45b85470069dab
Jake McCray <jakemccray1@gmail.com>
2024-02-25 14:49:24 -0700

    setup and tested code for connecting to the database

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,40 @@
 /*****************************************************
  *  Status:
  *
- *  At this point i am just working on getting the db
- *  working (using the rocket library).
- *  After that I will work on creating an account
+ * Was able to set up the database. Now I need code to
+ * create accounts (don't forget the index function
+ * contains mainly test code that should be deleted)
  * **************************************************/
+
+mod user;
+
+use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::{Connection, Database};
+
 #[macro_use]
 extern crate rocket;
 
-mod user;
+#[derive(Database)]
+#[database("gift_list")]
+struct Db(sqlx::SqlitePool);
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index(mut db: Connection<Db>) -> &'static str {
+    match {
+        sqlx::query("SELECT name FROM users")
+            .fetch_one(&mut **db)
+            .await
+            .unwrap()
+            .is_empty()
+    } {
+        true => "Hello World",
+        false => "cannot find anything in db",
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .attach(Db::init())
+        .mount("/", routes![index])
 }
