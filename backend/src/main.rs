608046f69f240ad20240b65c2374789ba2fe5001
Jake McCray <jakemccray1@gmail.com>
2024-02-25 09:50:08 -0700

    got started on the user class, preparing to start on the database connections so that i can make a create account function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,14 @@
+/*****************************************************
+ *  Status:
+ *
+ *  At this point i am just working on getting the db
+ *  working (using the rocket library).
+ *  After that I will work on creating an account
+ * **************************************************/
 #[macro_use]
 extern crate rocket;
 
+mod user;
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
--- backend/src/user.rs
+++ backend/src/user.rs
@@ -1,3 +1,29 @@
-use sqlite;
+use argon2::Argon2;
 
-struct
+enum UserError {
+    UsernameExists,
+    EmailExists,
+}
+
+struct User {
+    username: String,
+    email: Option<String>,
+    p_hash: String,
+}
+
+impl User {
+    fn new(username: &str, email: &Option<String>, password: &str) -> Result<User, UserError> {
+        User::exists(username, email)?;
+        //hash password
+        //add user details to database
+        Ok(User {
+            username: String::from(username),
+            email: email.clone(),
+            p_hash: String::from(password),
+        })
+    }
+
+    fn exists(username: &str, email: &Option<String>) -> Result<(), UserError> {
+        Err(UserError::UsernameExists)
+    }
+}
